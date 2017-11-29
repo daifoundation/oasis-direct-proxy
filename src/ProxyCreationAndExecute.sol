5ee2a3510737930c5217348e24037eec2838665f
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2017-11-29 09:18:53 -0300

    Upgrade ds-proxy + use of build(owner)

--- src/ProxyCreationAndExecute.sol
+++ src/ProxyCreationAndExecute.sol
@@ -5,38 +5,33 @@ import "ds-proxy/proxy.sol";
 
 contract ProxyCreationAndExecute is OasisDirectProxy {
 
-    function createProxy(DSProxyFactory factory) internal returns (DSProxy proxy) {
-        proxy = factory.build();
-        proxy.setOwner(msg.sender);
-    }
-
     function createAndSellAllAmount(DSProxyFactory factory, OtcInterface otc, TokenInterface payToken, uint payAmt, TokenInterface buyToken, uint minBuyAmt) public returns (DSProxy proxy, uint buyAmt) {
-        proxy = createProxy(factory);
+        proxy = factory.build(msg.sender);
         buyAmt = sellAllAmount(otc, payToken, payAmt, buyToken, minBuyAmt);
     }
 
     function createAndSellAllAmountPayEth(DSProxyFactory factory, OtcInterface otc, TokenInterface wethToken, TokenInterface buyToken, uint minBuyAmt) public payable returns (DSProxy proxy, uint buyAmt) {
-        proxy = createProxy(factory);
+        proxy = factory.build(msg.sender);
         buyAmt = sellAllAmountPayEth(otc, wethToken, buyToken, minBuyAmt);
     }
 
     function createAndSellAllAmountBuyEth(DSProxyFactory factory, OtcInterface otc, TokenInterface payToken, uint payAmt, TokenInterface wethToken, uint minBuyAmt) public returns (DSProxy proxy, uint wethAmt) {
-        proxy = createProxy(factory);
+        proxy = factory.build(msg.sender);
         wethAmt = sellAllAmountBuyEth(otc, payToken, payAmt, wethToken, minBuyAmt);
     }
 
     function createAndBuyAllAmount(DSProxyFactory factory, OtcInterface otc, TokenInterface buyToken, uint buyAmt, TokenInterface payToken, uint maxPayAmt) public returns (DSProxy proxy, uint payAmt) {
-        proxy = createProxy(factory);
+        proxy = factory.build(msg.sender);
         payAmt = buyAllAmount(otc, buyToken, buyAmt, payToken, maxPayAmt);
     }
 
     function createAndBuyAllAmountPayEth(DSProxyFactory factory, OtcInterface otc, TokenInterface buyToken, uint buyAmt, TokenInterface wethToken) public payable returns (DSProxy proxy, uint wethAmt) {
-        proxy = createProxy(factory);
+        proxy = factory.build(msg.sender);
         wethAmt = buyAllAmountPayEth(otc, buyToken, buyAmt, wethToken);
     }
 
     function createAndBuyAllAmountBuyEth(DSProxyFactory factory, OtcInterface otc, TokenInterface wethToken, uint wethAmt, TokenInterface payToken, uint maxPayAmt) public returns (DSProxy proxy, uint payAmt) {
-        proxy = createProxy(factory);
+        proxy = factory.build(msg.sender);
         payAmt = buyAllAmountBuyEth(otc, wethToken, wethAmt, payToken, maxPayAmt);
     }
 }
